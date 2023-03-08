4007902e3a7e6a7fd865c2d0487773bdbec21ef6
Jeffrey Pincombe <pincombj@sheridancollege.ca>
2023-03-08 17:09:23 -0500

    First commit

--- src/midtermexam_versiona_extensioncode/UserProfile.java
+++ src/midtermexam_versiona_extensioncode/UserProfile.java
@@ -5,6 +5,10 @@
  */
 package midtermexam_versiona_extensioncode;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Scanner;
+
 /**
  * The user profile for the OnlineVids system. The user has a userID
  * and a favourite genre that must be one of the choices in the
@@ -16,9 +20,9 @@ package midtermexam_versiona_extensioncode;
 public class UserProfile 
 {
     
-    private String userID;//the userID
-    private String genre;// the user's preferred genre of movie
-    private String[] genres = {"Comedy", "Drama", "Action", "Mystery"};//available genres of movies
+    private static String userID;//the userID
+    private static String genre;// the user's preferred genre of movie
+    private static String[] genres = {"Comedy", "Drama", "Action", "Mystery"};//available genres of movies
     
     /**
      * A constructor that takes in the userID and the favourite genre
@@ -62,7 +66,45 @@ public class UserProfile
     }
     
     
-    
-    
-    
-}//end class
+    public class TestUserprofile{
+        public static void main(String[] args){
+            Scanner input = new Scanner(System.in);
+            System.out.println("Enter user ID:");
+            UserProfile userprofile = new UserProfile(userID, genre);
+            userprofile.userID = input.next();
+           // UserProfile genres = new UserProfile(genre, genre);
+            
+            //Scanner choose = new Scanner(System.in);
+            //genres.genre = choose.next();
+            
+            
+            //ArrayList<String> genres = new ArrayList<String>();
+            System.out.println(genres);
+            System.out.println("User ID is: " + input.next());
+            //System.out.println("Chosen genre is: " + choose.next());
+            //System.out.println("Choose a genre: " + g);
+
+            genre = userprofile.genres[0];
+            for (int i=0; i< genres.length; i++){
+                String g = genres[i];
+                System.out.println("Choose a genre with 1,2,3,or 4: " + g);
+
+            if (input.next() == "1"){
+                System.out.println(input.next() + " has chosen " + genres[0] + "User profile has been created");
+
+            }
+            else if (input.next() == "2"){
+                System.out.println(input.next() + " has chosen " + genres[1] + "User profile has been created");
+            }
+            else if (input.next() == "3") {
+                System.out.println(input.next() + " has chosen " + genres[2] + "User profile has been created");
+            }
+            else if (input.next() == "4") {
+                System.out.println(input.next() + " has chosen " + genres[3] + "User profile has been created");
+            }
+            else { System.out.println("Thats not an option");}
+            }
+                    
+        }
+    }
+    }
